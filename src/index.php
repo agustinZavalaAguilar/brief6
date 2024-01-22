6e887bec58db02f9312a56520f1d02159381db8f
agustinZavalaAguilar <88766456+agustinZavalaAguilar@users.noreply.github.com>
2024-01-22 15:47:00 +0100

    hover effect

--- src/index.php
+++ src/index.php
@@ -8,6 +8,8 @@
     <title>PHP PDO</title>
     <link href="./output.css" rel="stylesheet">
     <script src="https://kit.fontawesome.com/e14b518d69.js" crossorigin="anonymous"></script>
+    <!-----------font Roboto--------------->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap">
 </head>
 <body>
     <!---Cherche le fichier header, pour lisibilité---->
@@ -18,9 +20,9 @@
     <?php $result = $pdo->query("SELECT * FROM favoris /*WHERE prenom='julien'*/");
     $favoris = $result->fetchAll(PDO::FETCH_ASSOC);?>
     <section id="favoris">
-        <table class="table_favori">
+        <table class="table_favori m-10 ">
             <!--Titres du tablau----------------------->
-            <tr class="text-center text-blue-800 bg-gray-200 m-6">
+            <tr class="text-center text-blue-800 bg-gray-200 ">
                 <th>id favori</th>
                 <th class="text-center text-blue-800">Libellé</th>
                 <th>date creation</th>
@@ -30,15 +32,15 @@
             </tr>
             <!---Ligne exemple---->
             <?php foreach ($favoris as $favori) { ?>
-            <tr class="text-center bg-gray-100 border border-gray-200 font-normal m-6 border border-gray-200 mx-auto max-w-screen-md">
+            <tr class="text-center bg-gray-100 hover:bg-gray-200 border border-gray-200 font-normal border border-gray-200 mx-auto max-w-screen-md">
                 <th class="font-normal" ><?= $favori['id_favori'] ?></th>
                 <th class="font-normal" ><?= $favori['libelle'] ?></th>
                 <th class="font-normal" ><?= $favori['date_creation'] ?></th>
                 <th class="font-normal" ><?= $favori['url'] ?></th>
                 <th class="font-normal" ><?= $favori['id_domaine'] ?></th>
                 <th>
-                    <i class="fa-solid fa-pencil text-blue-800"></i>
-                    <i class="fa-solid fa-trash text-blue-800"></i>
+                    <i class="fa-solid fa-pencil text-blue-800 m-1"></i>
+                    <i class="fa-solid fa-trash text-blue-800 m-1"></i>
                 </th>
             </tr>
             <?php 
