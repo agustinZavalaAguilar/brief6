522485be445a6e3df688bb657c9b422e418f34af
agustinZavalaAguilar <88766456+agustinZavalaAguilar@users.noreply.github.com>
2024-01-22 15:19:44 +0100

    basic tailwind styling

--- src/header.php
+++ src/header.php
@@ -1,3 +1,3 @@
 <header>
-        <h1>Mes favoris</h1>
+        <h1 class="text-4xl font-bold text-blue-800 h-20 flex items-center justify-center ">Mes favoris</h1>
 </header>
--- src/index.php
+++ src/index.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP PDO</title>
     <link href="./output.css" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/e14b518d69.js" crossorigin="anonymous"></script>
 </head>
 <body>
     <!---Cherche le fichier header, pour lisibilité---->
@@ -19,21 +20,26 @@
     <section id="favoris">
         <table class="table_favori">
             <!--Titres du tablau----------------------->
-            <tr>
+            <tr class="text-center text-blue-800 bg-gray-200 m-6">
                 <th>id favori</th>
-                <th>Libellé</th>
+                <th class="text-center text-blue-800">Libellé</th>
                 <th>date creation</th>
                 <th>url</th>
                 <th>Id domaine</th>
+                <th>Actions</th>
             </tr>
             <!---Ligne exemple---->
             <?php foreach ($favoris as $favori) { ?>
-            <tr>
-                <th><?= $favori['id_favori'] ?></th>
-                <th><?= $favori['libelle'] ?></th>
-                <th><?= $favori['date_creation'] ?></th>
-                <th><?= $favori['url'] ?></th>
-                <th><?= $favori['id_domaine'] ?></th>
+            <tr class="text-center bg-gray-100 border border-gray-200 font-normal m-6 border border-gray-200 mx-auto max-w-screen-md">
+                <th class="font-normal" ><?= $favori['id_favori'] ?></th>
+                <th class="font-normal" ><?= $favori['libelle'] ?></th>
+                <th class="font-normal" ><?= $favori['date_creation'] ?></th>
+                <th class="font-normal" ><?= $favori['url'] ?></th>
+                <th class="font-normal" ><?= $favori['id_domaine'] ?></th>
+                <th>
+                    <i class="fa-solid fa-pencil text-blue-800"></i>
+                    <i class="fa-solid fa-trash text-blue-800"></i>
+                </th>
             </tr>
             <?php 
             } 
