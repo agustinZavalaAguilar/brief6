196d78598c652c04ec9376a0daa2b89889e4f319
agustinZavalaAguilar <88766456+agustinZavalaAguilar@users.noreply.github.com>
2024-01-24 16:54:58 +0100

    added text search

--- src/index.php
+++ src/index.php
@@ -35,7 +35,7 @@
         $requeteSQL= "SELECT * FROM favoris";
     };
     /*var_dump($requeteSQL);*/
-    var_dump($_GET);
+    /*var_dump($_GET);*/
     $result = $pdo->query($requeteSQL);
     $favoris = $result->fetchAll(PDO::FETCH_ASSOC);
 
@@ -51,11 +51,10 @@
         $result = $pdo->query("SELECT * FROM domaine");
         $domaines = $result->fetchAll(PDO::FETCH_ASSOC);
     ?>
-
-    <div class="flex justify-center block bg-gray-100">
-    <!--Row avec les boutons de tri------------>          
-        
+    <!----------Options de tri et recherche----------------------------->
+    <div class="flex justify-center block bg-gray-100">    
         <form class="flex flex-row items-center" action="" method="GET">
+            <!--Options de tri par categorie --------------------------> 
             <div class="flex flex-col p-10 m-10">
                 <h2>Selectionnez une catégorie</h2>             
                 <select name="filtreCategorie" >
@@ -71,11 +70,13 @@
                 ?>                
                 </select>
             </div>
-            <!-----------Tri par domaine---------------------------->
+            <!----Options de tri par domaine---------------------------->
             <div class="flex flex-col p-10 m-10">
-                <h2>Selectionnez un domaine</h2>
-            
+                <h2>Selectionnez un domaine</h2>            
                 <select name="filtreDomaine" > 
+
+                <option value="">-- toutes --</option>
+
                 <?php foreach ($domaines as $domaine) { ?>            
                     <option class="font-normal" value="<?php echo $domaine['id_domaine']?>"><?= $domaine['nom_domaine'] ?></option>
                 <?php 
@@ -85,7 +86,14 @@
             </div>
             <button class="font-bold bg-blue-400 hover:bg-blue-900 text-white px-4 py-2 rounded h-10 ml-20 border border-gray-300 shadow-lg" 
                 type="submit">Filtrer</button>  
-        </form>          
+        </form>
+                 
+    </div>
+    <!--------------------recherche textuelle---------------------------------->
+    <div class="flex justify-center block bg-gray-100 h-10 ">
+        <form>
+            <input class="rounded  ml-20" type="search" name="search" placeholder="Écris ici ta recherche">
+        </form>         
     </div>
 
     <section id="favoris" class="flex justify-center">      
